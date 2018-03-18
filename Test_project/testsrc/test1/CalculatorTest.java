399053db728cace8b55e5319cab3b9fffdf53115
Ghaiath <gm.trabulsi@gmail.com>
2018-03-18 18:50:04 +0100

    Create JUnit test class

---
+++ Test_project/testsrc/test1/CalculatorTest.java
@@ -0,0 +1,24 @@
+package test1;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void testSubstract() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAdd() {
+		fail("Not yet implemented");
+	}
+
+}
